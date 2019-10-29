7d6e4a835407da1f23951ce2a0ecd66cfdb59036
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-29 20:40:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # self[-1] == "."
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    delimiters = ['! ', '. ', "? "]
+    new_array = self.split(Regexp.union(delimiters))
+    return new_array.count 
   end
 end
