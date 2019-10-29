c4c4db20453b4179261b10de0a8a9623f06a9cd7
Griffin Poole <griffinpoole5@gmail.com>
2019-10-29 12:56:46 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == '.' ? true : false
   end
 
   def question?
-
+    self[-1] == '?' ? true : false
   end
 
   def exclamation?
-
+    self[-1] == '!' ? true : false
   end
 
   def count_sentences
-
+    words = self.split(' ')
+    binding.pry
+    sentences = words.select{ |word|
+      word.sentence? || word.question? || word.exclamation?
+    }
+    sentences.length
   end
 end
